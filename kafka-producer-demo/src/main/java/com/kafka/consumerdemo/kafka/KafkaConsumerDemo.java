5edddd72e6d744042c85347733c53a53fbbccc92
grootenthu <amit.patil60@yahoo.in>
2022-01-19 13:48:44 +0530

    Added new class which reads data from topic and works as consumer

---
+++ kafka-producer-demo/src/main/java/com/kafka/consumerdemo/kafka/KafkaConsumerDemo.java
@@ -0,0 +1,50 @@
+package com.kafka.consumerdemo.kafka;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaConsumerDemo {
+
+	public static void main(String[] args) {
+
+		final Logger logger = LoggerFactory.getLogger(KafkaConsumerDemo.class);
+		String bootstrapServers = "localhost:9092";
+		String groupId = "my-java-app";
+
+		// create producer properties
+		Properties properties = new Properties();
+		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+		// create consumer
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+		// subscribe consumer to our topic(s)
+		consumer.subscribe(Arrays.asList("first_topic"));
+
+		// poll for new data
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+			for (ConsumerRecord<String, String> record : records) {
+				logger.info("Recieved record" + "\n");
+				logger.info("Key : " + record.key() + " , Value : " + record.value() + " , Partition : "
+						+ record.partition());
+				;
+			}
+		}
+	}
+}
